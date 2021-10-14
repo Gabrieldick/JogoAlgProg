ee9d6f227e6c13b7580da415decc9a9532874767
Gabrieldick <91341336+Gabrieldick@users.noreply.github.com>
2021-10-13 23:02:54 -0300

    Update main.c

--- TesteJogo/main.c
+++ TesteJogo/main.c
@@ -4,32 +4,32 @@
 #include <time.h>
 #include <windows.h>
 #include <stdlib.h>
-#define TamX 11
-#define TamY 11
-
+#define TamX 12
+#define TamY 12
 
 int main(void)
 {
     char mapa[TamX][TamY] =
     {
-        {"##########"},
-        {"#O      I#"},
-        {"# ###### #"},
-        {"# ###### #"},
-        {"#  I#S## #"},
-        {"### #I   #"},
-        {"### #### #"},
-        {"### ####I#"},
-        {"#SI    IT#"},
-        {"##########"}
+        {"###########"},
+        {"###########"},
+        {"##O      I#"},
+        {"## ###### #"},
+        {"## ###### #"},
+        {"##  I#S## #"},
+        {"#### #I   #"},
+        {"#### #### #"},
+        {"#### ####I#"},
+        {"##SI    IT#"},
+        {"###########"}
     };
 
 
     int ch = 0;
-    int x = 1, y = 1;
+    int x = 2, y = 2;
 
     textcolor(WHITE);
-    montaMapa(mapa);
+    montaMapa(&mapa);
 
     while (ch != 27)
     {
