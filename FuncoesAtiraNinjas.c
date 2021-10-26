c87e042caf02adcdc81e0c7b0dd273f03a8b433f
rsrsaaa <48812143+rsrsaaa@users.noreply.github.com>
2021-10-26 17:18:25 -0300

    Update FuncoesAtiraNinjas.c

--- FuncoesAtiraNinjas.c
+++ FuncoesAtiraNinjas.c
@@ -1,3 +1,111 @@
+void anda_ninjas(int NINJAx[], int NINJAy[], char mapa[LIN][COL], int ninja_morto[], int *x, int *y, int *flag_ninja, TEMPO tempo, int *vidas)
+{
+    int mov_ninja, valid_move, n = 0;
+    for (n = 0; n < QtdNinjas; n++)
+    {
+        if (ninja_morto[n] == 0)
+        {
+
+                if(*y == NINJAy[n])
+                {
+                    if (flag_ninja==1)
+                    {
+                        tempo.comeco = clock();
+                        flag_ninja = 0;
+                    }
+
+
+                    if(*x > NINJAx[n])
+                    {
+                        printf("%i", *vidas);
+                        atira_ninjas(NINJAx[n], NINJAy[n], 32, 77, mapa, x, y, NINJAx, NINJAy, ninja_morto, &tempo, &flag_ninja, vidas);
+                    }
+                    else
+                    {
+                        printf("%i", *vidas);
+                        atira_ninjas(NINJAx[n], NINJAy[n], 32, 75, mapa, x, y, NINJAx, NINJAy, ninja_morto, &tempo, &flag_ninja, vidas);
+                    }
+
+                }
+
+            do
+            {
+                mov_ninja = round(1 + ((float)rand() / RAND_MAX) * (4 - 1));
+                switch (mov_ninja)
+                {
+                    case 1: // a
+                        if (mapa[NINJAy[n] - 1][NINJAx[n] - 2] != '#')
+                        {
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(BLACK);
+                            printf(" ");
+                            NINJAx[n] = NINJAx[n] - 1;
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(DARKGRAY);
+                            printf(" ");
+                            valid_move = 1;
+                        }
+                        else
+                            valid_move = 0;
+                        break;
+
+                    case 2: // d
+                        if (mapa[NINJAy[n] - 1][NINJAx[n]] != '#')
+                        {
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(BLACK);
+                            printf(" ");
+                            NINJAx[n] = NINJAx[n] + 1;
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(DARKGRAY);
+                            printf(" ");
+                            valid_move = 1;
+                        }
+                        else
+                            valid_move = 0;
+                        break;
+
+                    case 3: // s
+                        if (mapa[NINJAy[n]][NINJAx[n] - 1] != '#')
+                        {
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(BLACK);
+                            printf(" ");
+                            NINJAy[n] = NINJAy[n] + 1;
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(DARKGRAY);
+                            printf(" ");
+                            valid_move = 1;
+                        }
+                        else
+                            valid_move = 0;
+                        break;
+
+                    case 4: // w
+                        if (mapa[NINJAy[n] - 2][NINJAx[n] - 1] != '#')
+                        {
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(BLACK);
+                            printf(" ");
+                            NINJAy[n] = NINJAy[n] - 1;
+                            gotoxy(NINJAx[n], NINJAy[n]);
+                            textbackground(DARKGRAY);
+                            printf(" ");
+                            valid_move = 1;
+                        }
+                        else
+                            valid_move = 0;
+                        break;
+
+                    default:
+                        valid_move = 0;
+                        break;
+                }
+            } while (valid_move == 0);
+        }
+    }
+}
+
 void atira_ninjas(int xb, int yb, int ch, int prox_ch, char mapa[LIN][COL], int *x, int *y, int NINJAx[], int NINJAy[], int ninja_morto[], TEMPO *tempo, int *flag_ninja, int *vidas)
 {
     int i, n;
@@ -18,8 +126,9 @@ void atira_ninjas(int xb, int yb, int ch, int prox_ch, char mapa[LIN][COL], int 
                     printf(" ");
                         if (xb == *x && yb == *y)
                         {
-                            *vidas--;
-                            printf("teste");
+
+                            printf("%d", *vidas);
+
                         }
                     xb++;
                 }
@@ -27,7 +136,7 @@ void atira_ninjas(int xb, int yb, int ch, int prox_ch, char mapa[LIN][COL], int 
                 tempo->duracao = (double)(tempo->fim - tempo->comeco) / CLOCKS_PER_SEC;
                 if (tempo->duracao >= TIME_GAME)
                 {
-                    anda_ninjas(NINJAx, NINJAy, mapa, ninja_morto);
+
                     *flag_ninja = 1;
                     tempo->comeco = clock();
                 }
@@ -54,8 +163,9 @@ void atira_ninjas(int xb, int yb, int ch, int prox_ch, char mapa[LIN][COL], int 
                     printf(" ");
                         if (xb == *x && yb == *y)
                         {
-                            *vidas--;
-                            printf("teste");
+
+                            printf("%d", *vidas);
+
                         }
                     xb--;
                 }
@@ -63,7 +173,7 @@ void atira_ninjas(int xb, int yb, int ch, int prox_ch, char mapa[LIN][COL], int 
                 tempo->duracao = (double)(tempo->fim - tempo->comeco) / CLOCKS_PER_SEC;
                 if (tempo->duracao >= TIME_GAME)
                 {
-                    anda_ninjas(NINJAx, NINJAy, mapa, ninja_morto);
+
                     *flag_ninja = 1;
                     tempo->comeco = clock();
                 }
